18e0a36d29f2a0036fede118d42b50be9e3c5d58
MaksMusic <Maksmusic05@yandex.ru>
2023-06-06 15:01:38 +0300

    Initial commit

---
+++ src/main/java/Connect.java
@@ -0,0 +1,44 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+
+public class Connect {
+    public static void main(String[] args) {
+
+        try {
+           var doc =  Jsoup.connect("http://www.planet-online.ru/").get();
+           var title = doc.selectFirst("title").text();
+
+            //заголовки
+           var elements = doc.select("h3");
+            for (Element element : elements) {
+                System.out.println(element.text());
+            }
+
+            System.out.println();
+            //заголовки всех ссылок
+            var elements2 = doc.select("a");
+            for (Element element2 : elements2) {
+                System.out.println(element2.text());
+            }
+
+            //сами ссылки
+            var elements3 = doc.select("a");
+            for (Element element3 : elements3) {
+                System.out.println(element3.attr("href"));
+            }
+
+
+            //получить текст по id
+            System.out.println();
+            var elements4 = doc.select("#content");
+            for (Element element : elements4) {
+                System.out.println(element.text());
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/Connect2.java
@@ -0,0 +1,50 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+public class Connect2 {
+    public static void main(String[] args) {
+        Document doc = null;
+        try {
+            doc = Jsoup.connect("https://www.gismeteo.ru/weather-london-744/").get();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        if (doc!= null){
+            var title = doc.selectFirst("title").text();
+            System.out.println("title " + title);
+
+            System.out.println();
+            var elements = doc.select("a");
+            for (Element element : elements) {
+                System.out.println(element.text());
+            }
+
+            System.out.println();
+            for (Element element : elements) {
+                System.out.println(element.attr("href"));
+            }
+
+
+            //по классу
+            System.out.println("id");
+            var elementsClass = doc.select(".unit_temperature_c");
+            for (Element element : elementsClass) {
+                System.out.println(element.text());
+            }
+
+            System.out.println();
+            var elementsClass2 = doc.select(".page-title");
+            for (Element element : elementsClass2) {
+                System.out.println(element.text());
+            }
+
+            //или если в классе 1 element
+            System.out.println(doc.selectFirst(".page-title").text());
+
+        }
+
+        System.out.println();
+    }
+}
